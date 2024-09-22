07680932daa2204f33d6e2d53d9b325c285e419a
Anderson Souza <Anderson1231993@hotmail.com>
2024-09-22 15:18:59 -0300

    refactor: refactoring main

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 import os
 from PIL import Image
+from colors import *
+from geometric_shapes import *
 
 IMG_DIR = 'img'
 filename = os.path.join(IMG_DIR, 'brasil.png')
 
-GREEN = (0, 148, 64)
-YELLOW = (255, 203, 0)
-BLUE = (48, 38, 129)
-WHITE = (255, 255, 255)
-
 WIDTH = 1400
-LENGTH = int(WIDTH / 14 * 20)
+module = WIDTH / 14
+LENGTH = int(module * 20)
 
 img = Image.new("RGB", (LENGTH, WIDTH), GREEN)
-img.save(os.path.join(IMG_DIR, "brasil_step_one.png"))
+paint_diagonally(img, YELLOW)
+img.save(os.path.join(IMG_DIR, "brasil_step_two.png"))
+img.show()
